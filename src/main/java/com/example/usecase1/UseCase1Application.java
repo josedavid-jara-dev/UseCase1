660e72960923f9b971c4044b395bbfc88f101a50
Jose David <jdavi@softserveinc.com>
2023-08-31 11:59:20 +0300

    UC#1 Infrastructure setup.

---
+++ src/main/java/com/example/usecase1/UseCase1Application.java
@@ -0,0 +1,13 @@
+package com.example.usecase1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseCase1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UseCase1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/usecase1/UseCase1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usecase1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UseCase1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
